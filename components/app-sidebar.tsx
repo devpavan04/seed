af6efa0229657f347303820eb4ec56b991b09a65
Pavan Soratur <pavanvsoratur@gmail.com>
2026-01-13 14:58:33 -0800

    style: make sidebar inset variant

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -85,7 +85,7 @@ const navMain: NavMainItem[] = [
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="icon" {...props}>
+    <Sidebar variant="inset" collapsible="icon" {...props}>
       <SidebarHeader>
         <SidebarLogo />
       </SidebarHeader>
